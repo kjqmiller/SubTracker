09028ca6fc71e8761896451603ad23ccd3bd9bb1
kjqmiller <56325660+kjqmiller@users.noreply.github.com>
2021-11-05 15:51:37 -0500

    Update main.py
    Cleaned up and updated

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-#!/usr/local/bin/python3.9
 from call_reddit_api import get_data
 from clean_and_organize_data import update_dictionary
 from load_data import load_data
 
-# Select a subreddit
-subreddit = 'Superstonk'
-# Get dictionary of data from subreddit
-data = get_data(subreddit)
-# Update dictionary with any transformations or additional calculations
-updated_data = update_dictionary(data)
-# Convert dictionary to CSV and append
-# Check for duplicate data, if so rerun from beginning
-load_data(updated_data)
-print('==========================')
+
+def Handler(event=None, context=None):
+    # Select a subreddit without the "/r" (examples: 'learnpython', 'pics', 'Python', 'stocks', 'LifeProTips')
+    subreddit = 'subreddit_to_track'
+
+    # Get dictionary of data from chosen subreddit
+    data = get_data(subreddit)
+
+    # Update dictionary with any transformations or additional calculations
+    updated_data = update_dictionary(data)
+
+    # Stage data in s3 bucket, then append staged data to current CSV, or create new CSV if none exists
+    load_data(updated_data)
