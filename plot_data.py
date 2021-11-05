3273915fe483780bf56526933a1bcecf8249604c
kjqmiller <56325660+kjqmiller@users.noreply.github.com>
2021-11-05 16:43:51 -0500

    Update plot.py
    Fixed bug where all plots had same date in titles. Added line to allow option to save plot as well as show it in window. Line to show it currently commented out. Clarified variable name for s3 path.

--- plot_data.py
+++ plot_data.py
@@ -8,13 +8,14 @@ import matplotlib.dates as mdates
 import numpy as np
 
 """
-Plot data for a chosen date, will open graphs in a new windown on your local machine. Currently this is the only
-script in the project that needs to be ran locally.
+Plots data, saves it to a file locally and/or opens it in a local window. Can choose between "today", "yesterday", or "custom_date" for data to plot.
+Choose on line 29 inside str() function
 """
 # Choose a date that you'd like to plot the data for. Files stored in s3 are named with 'YYYY-MM-DD.csv' format
 today = datetime.datetime.utcnow().date()
 yesterday = today - datetime.timedelta(days=1)
-custom_date = '2021-10-31'  # Format is 'YYYY-MM-DD'
+custom_date = '2021-11-01'  # Format is 'YYYY-MM-DD'
+title_date = None
 
 # Create arrays with values for x and y axes for each data sets
 online_y = []
@@ -22,17 +23,21 @@ subscribers_y = []
 time_object_x = []
 percent_online = []
 
-# Connect to s3 and then open the desired CSV and append the data to the above arrays for plotting
+# Connect to s3 and then open the desired CSV on line 25 and append the data to the above arrays for plotting
 s3_client = boto3.client('s3')
 bucket = 'your_bucket'
-staged_csv_path = 'data_folder/' + str(yesterday) + '.csv'
+csv_to_plot_path = 'data_folder/' + str(custom_date) + '.csv'
 downloaded_csv_name = 'data.csv'
 try:
-    today_csv = s3_client.download_file(bucket, staged_csv_path, downloaded_csv_name)
+    today_csv = s3_client.download_file(bucket, csv_to_plot_path, downloaded_csv_name)
     with open('data.csv') as file:
         reader = csv.reader(file)
         next(reader)
+        first = True
         for row in reader:
+            if first:
+                title_date = row[3]
+                first = False
             subscribers_y.append(int(row[0]))
             online_y.append(int(row[1]))
             timestamp = row[2]
@@ -53,7 +58,7 @@ try:
         plt.xticks(rotation=90)
         plt.xlabel('UTC Time')
         plt.ylabel('Total Subs vs Online Subs')
-        plt.title('Total vs Online Subs for ' + str(today))
+        plt.title('Total vs Online Subs for ' + str(title_date))
         plt.grid(axis='y')
         plt.legend()
 
@@ -64,7 +69,7 @@ try:
         plt.xticks(rotation=90)
         plt.xlabel('UTC Time')
         plt.ylabel('Total Subs')
-        plt.title('Total Subs for ' + str(today))
+        plt.title('Total Subs for ' + str(title_date))
         plt.grid(axis='y')
         plt.grid(axis='x')
         plt.legend()
@@ -76,7 +81,7 @@ try:
         plt.xticks(rotation=90)
         plt.xlabel('UTC Time')
         plt.ylabel('Online Subs')
-        plt.title('Online Subs for ' + str(today))
+        plt.title('Online Subs for ' + str(title_date))
         plt.grid(axis='y')
         plt.legend()
 
@@ -91,7 +96,7 @@ try:
         plt.xticks(rotation=90)
         plt.xlabel('UTC Time')
         plt.ylabel('Percent Online')
-        plt.title('Percentage of Subscribers Currently Online for ' + str(today))
+        plt.title('Percentage of Subscribers Currently Online for ' + str(title_date))
         plt.grid(axis='y', color='k')
         plt.legend()
 
@@ -99,7 +104,8 @@ try:
         plt.subplots_adjust(left=0.2, bottom=0.2, right=None, top=None, wspace=None, hspace=1)
 
         plt.tight_layout()
-        plt.show()
+        plt.savefig('/path/to/file/' + str(title_date) + '.jpg')
+        # plt.show()
 
 except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == "404":
