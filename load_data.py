52424570c47a11bf3dac2165bcd2246e074e7901
kjqmiller <56325660+kjqmiller@users.noreply.github.com>
2021-11-05 15:49:40 -0500

    Update load_data.py
    Cleaned up and updated

--- load_data.py
+++ load_data.py
@@ -1,50 +1,54 @@
 import csv
 import boto3
 import pandas as pd
-from datetime import datetime
 import botocore.exceptions
+from datetime import datetime
 
-
-# Get dictionary from previous function and convert to DataFrame. Then convert DF into CSV and either append to current
-# day's CSV file, or create a new one for the first read of the day.
-def load_data(main_updated_dict):
+"""
+Get dictionary from previous function and convert to DataFrame. Then convert DF into CSV and either append to current
+day's CSV file, or create a new one for the first read of the day. Finally, upload final product to s3.
+"""
+def load_data(data_dict):
     # Connect to s3
-    s3 = boto3.resource('s3')
     s3_client = boto3.client('s3')
+    print('Connected to s3')
 
     # Stage data as CSV in s3
     # In order to upload directly to s3, s3fs must be installed. s3fs builds upon botocore and fsspec.
-    df = pd.DataFrame([main_updated_dict])
-    df.to_csv('s3://reddit-user-data/staging_data/staging.csv', index=False)
+    df = pd.DataFrame([data_dict])
+    df.to_csv('s3://your_bucket/staging_data_folder/staging_file.csv', index=False)
+    print('Staged CSV')
 
-    # Function arguments
-    bucket = 'reddit-user-data'
+    # Arguments to download from s3 staging to Lambda /tmp/ folder
+    bucket = 'your_bucket'
+    staged_s3_path = 'staging_data_folder/staging_file.csv'
+    lambda_staged_csv_path = '/tmp/staging_file.csv'
 
-    # Get staged CSV - works
-    staged_csv_path = 'staging_data/staging.csv'
-    # TODO eventually the following should be in a lambda /tmp/ folder
-    staged_csv_name = 'staging.csv'
-    s3_client.download_file(bucket, staged_csv_path, staged_csv_name)
+    # Download staged CSV to /tmp/ directory in Lambda
+    s3_client.download_file(bucket, staged_s3_path, lambda_staged_csv_path)
+    print('Retrieved staged CSV')
 
-    # Arguments
+    # Arguments to try and append to current day CSV or create new CSV for the day
     today_date = str(datetime.utcnow().date())
-    today_csv_path = ('data/' + today_date + '.csv')
-    today_csv_name = (today_date + '.csv')
-    s3_data_path = ('data/' + today_date + '.csv')
-    # Try to download csv with current date from s3 and append staging.csv, if it fails create new one and append
+    today_s3_path = ('data_folder/' + today_date + '.csv')
+    today_lambda_csv_name = ('/tmp/' + today_date + '.csv')
+    s3_data_path = ('data_folder/' + today_date + '.csv')
+
+    # Try to download CSV with current date from s3 and append staging.csv, if it fails create new CSV for today
     try:
-        s3_client.download_file(bucket, today_csv_path, today_csv_name)
-        with open(staged_csv_name, 'r') as staged:  # Open staged.csv, skip the header, then read the remaining
+        s3_client.download_file(bucket, today_s3_path, today_lambda_csv_name)
+        with open(lambda_staged_csv_path, 'r') as staged:  # Open staged.csv, skip the header, then read the remaining
             reader = csv.reader(staged)
             next(reader)
             staged_csv = staged.read()
-        with open(today_csv_name, 'a') as today:  # Open today.csv and append the read portion of staged.csv to it
+        with open(today_lambda_csv_name, 'a') as today:  # Open today.csv and append the read portion of staged.csv
             today.write(staged_csv)
-        s3_client.upload_file(today_csv_name, bucket, s3_data_path)  # Upload today.csv to s3
-    # If there is no existing today.csv, create a new one using staged.csv
+        s3_client.upload_file(today_lambda_csv_name, bucket, s3_data_path)  # Upload today.csv to s3
+    # If there is no existing today.csv  except block will create a new CSV for today using staged.csv
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
-            print("Today's CSV does not exist. Creating a new one with staged.csv.")
-            s3_client.upload_file(staged_csv_name, bucket, s3_data_path)  # Upload staged.csv as today.csv
+            print("Today's CSV does not exist. Creating a new one.")
+            # s3_client.upload_file(staged_csv_name, bucket, s3_data_path)  # Upload staged.csv as today.csv
+            s3_client.upload_file(lambda_staged_csv_path, bucket, s3_data_path)  # Upload staged.csv as today.csv
         else:
             raise
